169aa2809ef0911e70074eafda9a0b49ea18ec8a
marcusbarbu <mgb.barbu@gmail.com>
2019-01-09 11:18:24 -0500

    stated working on CMake, ran clang-format on sourcecode

--- cpp/receiver.cpp
+++ cpp/receiver.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
-#include <cstdlib>
-#include <string>
-#include <cstring>
-#include <cctype>
-#include <thread>
-#include <chrono>
 #include "mqtt/client.h"
+#include <cctype>
+#include <chrono>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <thread>
 
 const std::string SERVER("tcp://127.0.0.1:1883");
 const std::string CLIENT_ID("cppTestReceiver");
@@ -14,52 +14,54 @@ const std::string TOPIC1("hello");
 const std::string TOPIC2("world");
 
 class callback : public virtual mqtt::callback {
-    mqtt::client& base_client;
-    
-    void connected(const std::string& message) override {
-        std::cout << std::endl << "Connected: " << message << std::endl;
-    }
-    
-    void connection_lost(const std::string& error) override {
-        if(!error.empty()){ 
-            std::cout << std::endl << "Error: " << error << std::endl;
-        }
-    }
+  mqtt::client &base_client;
 
-    void message_arrived(mqtt::const_message_ptr msg) override {
-        std::cout << msg->get_topic() << ": " << msg->get_payload_str() << std::endl;
+  void connected(const std::string &message) override {
+    std::cout << std::endl << "Connected: " << message << std::endl;
+  }
+
+  void connection_lost(const std::string &error) override {
+    if (!error.empty()) {
+      std::cout << std::endl << "Error: " << error << std::endl;
     }
+  }
 
-    void delivery_complete(mqtt::delivery_token_ptr token) override {}
+  void message_arrived(mqtt::const_message_ptr msg) override {
+    std::cout << msg->get_topic() << ": " << msg->get_payload_str()
+              << std::endl;
+  }
 
-    public:
-        callback(mqtt::client& cli) : base_client(cli) {}
+  void delivery_complete(mqtt::delivery_token_ptr token) override {}
+
+public:
+  callback(mqtt::client &cli) : base_client(cli) {}
 };
 
-int main(int argc, char* argv[]){
-    mqtt::connect_options options;
-    
-    options.set_keep_alive_interval(10);
-    options.set_clean_session(false);
-    options.set_automatic_reconnect(true);
-    
-    mqtt::client cli(SERVER, CLIENT_ID);
+int main(int argc, char *argv[]) {
+  mqtt::connect_options options;
 
-    callback cb(cli);
-    cli.set_callback(cb);
+  options.set_keep_alive_interval(10);
+  options.set_clean_session(false);
+  options.set_automatic_reconnect(true);
 
-    try {
-        std::cout << "Connecting to server " << SERVER << std::flush;
-        cli.connect(options);
-        cli.subscribe(TOPIC, 0);
-        cli.subscribe(TOPIC1, 0);
-        cli.subscribe(TOPIC2, 0);
-        std::cout << "all OK" << std::endl;
-        std::cout << "connected" << std::endl;
-    }
-    catch (const mqtt::exception& exc){
-        std::cout << "we errored: " << exc.what() << std::endl;
-        return 1;}
+  mqtt::client cli(SERVER, CLIENT_ID);
 
-    while (true); 
+  callback cb(cli);
+  cli.set_callback(cb);
+
+  try {
+    std::cout << "Connecting to server " << SERVER << std::flush;
+    cli.connect(options);
+    cli.subscribe(TOPIC, 0);
+    cli.subscribe(TOPIC1, 0);
+    cli.subscribe(TOPIC2, 0);
+    std::cout << "all OK" << std::endl;
+    std::cout << "connected" << std::endl;
+  } catch (const mqtt::exception &exc) {
+    std::cout << "we errored: " << exc.what() << std::endl;
+    return 1;
+  }
+
+  while (true)
+    ;
 }
